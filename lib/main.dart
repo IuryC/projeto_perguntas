3f90af079ac954b203d82a2e9b5fca99149c2514
IuryC <iurycauan0352@hotmail.com>
2023-09-17 11:39:44 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+
+  void responder() {
+    print("Pergunta respondida");
+  }
+  @override
+  Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      "Qual é a sua cor favorita?",
+      "Qual o seu animal favorito?",
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            ElevatedButton(
+              onPressed: () {
+                responder();
+              },
+              child: Text("Resposta 1"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                responder();
+              },
+              child: Text("Resposta 2"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                responder();
+              },
+              child: Text("Resposta 3"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
